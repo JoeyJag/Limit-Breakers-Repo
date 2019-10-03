bdd0abccecd85447d406682b1541d68e72d59ad3
Joel Jagarath <joey.jagarath05@gmail.com>
2019-10-03 22:51:09 +0200

    Initial commit

---
+++ Limit Breakers Repo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Limit_Breakers_Repo.MvcApplication" Language="C#" %>
---
+++ Limit Breakers Repo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Limit_Breakers_Repo.Startup))]
+namespace Limit_Breakers_Repo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
