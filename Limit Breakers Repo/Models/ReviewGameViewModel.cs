ff2ccd121fdaca9aa01f2b93957e64c22bc69630
Nevermore46 <54313017+Nevermore46@users.noreply.github.com>
2019-10-08 02:15:00 +0200

    asd

---
+++ Limit Breakers Repo/Models/ReviewGameViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Limit_Breakers_Repo.Models
+{
+    public class ReviewGameViewModel
+    {
+        [Key]
+        public int GameID { get; set; }
+        public string Name { get; set; }
+        public string ReviewOfGame { get; set; }
+        public int Rating { get; set; }
+        
+
+    }
+}
