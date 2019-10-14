b0079462066110e5ef59c5dc6f0fe3c62bb763f4
Ratul0307 <ratulramdhani0307@gmail.com>
2019-10-14 22:36:34 +0200

    css update, different backgrounds, search bar and edits to index tables

--- Limit Breakers Repo/App_Start/BundleConfig.cs
+++ Limit Breakers Repo/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Limit_Breakers_Repo
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-darkly.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
